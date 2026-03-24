206e44a8d21df71a6d0e19890aa6bfd4029bc000
MattiDragon <matias.koivikko@gmail.com>
2026-03-24 16:44:53 +0200

    Fix some issues with async
    - Locks didn't properly pass return values
    - Weird crashing with `runWithFinally`
    - Incorrect namespace used for some types

--- src/main/java/dev/mattidragon/jsonpatcher/misc/ModInternals.java
+++ src/main/java/dev/mattidragon/jsonpatcher/misc/ModInternals.java
@@ -1,17 +1,21 @@
 package dev.mattidragon.jsonpatcher.misc;
 
-import java.util.function.Supplier;
+import dev.mattidragon.jsonpatcher.lang.runtime.value.Value;
 
 @SuppressWarnings("unused")
 public class ModInternals {
     private ModInternals() {
     }
 
-    public static Object runWithFinally(Supplier<Object> main, Runnable last) {
+    public static Value runWithFinally(ValueSupplier main, Runnable last) {
         try {
             return main.get();
         } finally {
             last.run();
         }
+    }
+
+    public interface ValueSupplier {
+        Value get();
     }
 }
